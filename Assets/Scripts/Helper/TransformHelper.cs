52f50f5713d25d5c341ce063cf4b4b1f18706074
Martin Wilsdorf <de.marwils@gmail.com>
2025-08-04 22:27:48 +0200

    make helper class static

--- Assets/Scripts/Helper/TransformHelper.cs
+++ Assets/Scripts/Helper/TransformHelper.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class TransformHelper
+public static class TransformHelper
 {
     public static void SetTransformation(Transform sourceTransformation, Transform targetTransformation, params TransformationType[] types)
     {
